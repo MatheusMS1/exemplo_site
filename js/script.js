867fe0bb5a28bf542a6ad1d64c82cca1e01db44b
matheus <msmatheus407@gmail.com>
2023-02-24 21:43:39 -0300

    adicionando elementos js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 alert('Olá!')
+
+//JavaScript: Deve-se criar variáveis com 'const' e 'let'
+
+const a = Number(prompt("Digite o valor de A"))
+const b = Number(prompt("Digite o valor de B"))
+
+//const lista = [a, b]
+
+let resultado = a+b
+console.log (resultado)
+
+resultado = a-b
+console.log (resultado)
+
+document.write(resultado)
